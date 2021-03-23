402ac451d0ab0738b369cadd41694914daa81a24
said <said.ladghem@gmail.com>
2021-03-23 17:33:56 +0100

    Modification mineur docker compose

--- internal/models/responses.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// Response contains all the propeties of a response.
-// It is used for error and standard success responses.
-type Response struct {
-	Ok      bool   `json:"ok"`
-	Code    int    `json:"code"`
-	Message string `json:"message,omitempty"`
-}
